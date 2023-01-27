432b0d484e6e1144f17d750d296cfe0abedcd1f3
iosbd <io232001@gmail.com>
2023-01-27 16:41:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Exercise: String handling 1
+* define a string called `string1` that has assigned the value `"Hello"`
+* define a string called `string2` that has assigned the value `"How are you?"`
+* take the first 2 characters `string1` and the last 2 characters of `string2` and combine the 4 `char`s (`Heu?`) into a dedicated array called `charArray`
+* print `charArray`
---
+++ src/test.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Array;
+
+public class test
+{
+    public static void main(String arg[])
+    {
+        String string1 = "Hello";
+        String string2 = "How are you?";
+        System.out.print(string1.charAt(0));
+        System.out.print(string1.charAt(1));
+        System.out.print(string2.charAt(10));
+        System.out.print(string2.charAt(11));
+    }
+}
+
